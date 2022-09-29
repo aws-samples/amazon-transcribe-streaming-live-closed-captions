2174b691089cdebebf49a3f11455c01aeebdcaba
George Vasels <gvasels@amazon.com>
2022-09-29 15:41:30 +0000

    modified start script to streamline number of ffmpeg actions and added a second parameter as input for script

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #/bin/bash
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
 unset input
+unset inputb
 unset output
 unset format
 unset delay
-while getopts i:o:f:d: flag
+while getopts i:b:o:f:d: flag
 do
     case "${flag}" in
         i) input=${OPTARG};;
+	b) inputb=${OPTARG};;
         o) output=${OPTARG};;
         f) format=${OPTARG};;
         d) delay=${OPTARG};;
@@ -16,6 +16,12 @@ do
 done
 
 if [ -z "$input" ]
+then
+   echo "Please provide an input, for example ./start.sh -i video.mp4"
+   echo "Example: ./start.sh -i rtmps://input-url/live/1234 -o rtmps://output-url/live/1234 -f flv -d 1.5"
+   exit
+fi
+if [ -z "$inputb" ]
 then
    echo "Please provide an input, for example ./start.sh -i video.mp4"
    echo "Example: ./start.sh -i rtmps://input-url/live/1234 -o rtmps://output-url/live/1234 -f flv -d 1.5"
@@ -39,5 +45,5 @@ then
    echo "Example: ./start.sh -i rtmps://input-url/live/1234 -o rtmps://output-url/live/1234 -f flv -d 1.5"
    exit
 fi
-ffmpeg -itsoffset $delay -loglevel quiet -re -i $input -c:v libx264 -profile:v main -preset veryfast -g 120 -x264opts "nal-hrd=cbr:no-scenecut" -acodec aac -ab 160k -ar 44100 -f flv - | flv+srt - transcript_fifo - | ffmpeg -loglevel quiet -y -i - -c:v copy -c:a aac -ar 44100 -ac 1 -metadata:s:s:0 language=eng -f $format $output &
+ffmpeg -itsoffset $delay -loglevel quiet -re -thread_queue_size 1024 -sn -i $inputb -c:v libx264 -profile:v main -preset veryfast -g 120 -x264opts "nal-hrd=cbr:no-scenecut" -acodec aac -ab 160k -ar 44100 -f flv - | flv+srt - transcript_fifo - | ffmpeg -loglevel quiet -y -i - -c:v copy -c:a copy -metadata:s:s:0 language=eng -f $format $output &
 ffmpeg -loglevel quiet -re -i $input -vn -ac 1 -c:a pcm_s16le -ar 16000 -f wav - | node index.js --fifo=transcript_fifo --stdin=true &
